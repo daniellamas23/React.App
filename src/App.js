8df6d3daa38d53ba81842735d2aa5fe5c844ae03
elmanya23 <dlamash23@gmail.com>
2022-10-29 16:00:08 -0300

    se actualiza basename de app.js y se agrega homepage en package.json

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter /*  basename='/React.App' */ >
+      <BrowserRouter basename='/React.App' >
         <CartProvider>
         <NavBar />       
         <Routes>
